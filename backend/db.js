fcad2fa66c1514c5527cb9e8fb6093ec87521145
meherabslays <136494220+meherabslays@users.noreply.github.com>
2024-12-14 20:28:10 +0600

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,29 @@
+// config/db.js
+const mysql = require('mysql');
+
+// Create the connection
+const db = mysql.createConnection({
+  host: 'localhost',  
+  user: 'meherab',    
+  password: 'meherab@7577',  
+  database: 'airline_reservation_system'
+});
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + db.threadId);
+
+  // Test query to ensure the connection is working
+  db.query('SELECT 1 + 1 AS solution', (err, result) => {
+    if (err) {
+      console.error('Test query failed:', err);
+      return;
+    }
+    console.log('Test query result:', result);
+  });
+});
+
+
+module.exports = db;
